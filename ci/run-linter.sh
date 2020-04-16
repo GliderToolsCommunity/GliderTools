5afee3cf30004ecefdc0ef65f94db2df08bb2c36
Luke Gregor <lukegre@gmail.com>
2020-04-16 23:08:39 +0200

    linter exclude flo flunctions

--- ci/run-linter.sh
+++ ci/run-linter.sh
@@ -10,7 +10,7 @@ source activate glidertools-dev
 # flake8 glidertools --exclude=__init__.py --max-line-length=79 --ignore=C901,W605,W503,F722
 
 echo "[black]"
-black --check -S -l 79 glidertools
+black --exclude flo_functions --check -S -l 79 glidertools
 
 echo "[isort]"
 isort --recursive --check-only -w 79 glidertools
