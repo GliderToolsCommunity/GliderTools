aa03a0ab84c3449c1998d01bb1e2715b0ca2278b
Luke Gregor <lukegre@gmail.com>
2020-10-11 12:31:16 +0200

    added test for failing

--- tests/test_optics.py
+++ tests/test_optics.py
@@ -1,12 +1,12 @@
+import pytest
+
+
 def test_sunrise_sunset():
     """
     Tests if sunrise/sunset:
         1. can run
         2. output is the right shape
         3. if the output is correct-ish
-
-    Note that the test will fail if the latitude is beyond where the
-    sun sets or rises. Perhaps we should add a length of day catch?
     """
     import numpy as np
 
@@ -26,3 +26,30 @@ def test_sunrise_sunset():
 
     # sunrise will be earlier for the SH in January
     assert sunrise[0] < sunrise[1]
+
+
+def test_sunrise_sunset_fail():
+    """
+    This is a test to make us aware that the astropy will fail if
+    the latitude is beyond where the sun sets or rises.
+    Perhaps we should add a length of day catch?
+    """
+    import numpy as np
+
+    import glidertools as gt
+
+    time = [
+        np.datetime64("2000-12-21"),
+        np.datetime64("2000-06-21"),
+    ]
+    lat = (
+        -80,
+        80,
+    )
+    lon = (
+        0,
+        0,
+    )
+
+    with pytest.raises(ValueError):
+        sunrise, sunset = gt.optics.sunset_sunrise(time, lat, lon)
