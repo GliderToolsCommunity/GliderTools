209005068180c1ed923bcca44171e233819e3e19
Julius Busecke <julius@ldeo.columbia.edu>
2021-03-30 09:45:03 -0400

    Oops

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     use_scm_version={
-        "write_to": "{{cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_')}}/_version.py",
+        "write_to": "glidertools/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
     }
