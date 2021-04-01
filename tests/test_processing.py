c0075fff071c9bdc74bf7f8c0aa9dbd2079802a9
Julius Busecke <julius@ldeo.columbia.edu>
2021-04-01 11:30:10 -0400

    Add dummy tests to test import dependencies.

---
+++ tests/test_processing.py
@@ -0,0 +1,13 @@
+from glidertools.processing import (
+    calc_physics,
+    calc_oxygen,
+    calc_backscatter,
+    calc_fluorescence,
+    calc_par,
+)
+
+"""WE REALLY NEED TO ADD TESTS!!! THESE JUST TEST THE BASIC IMPORT!!!"""
+
+
+def test_dummy():
+    assert 1 == 1
