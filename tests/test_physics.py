4547c00415be0bf22e33d49d361ed5b5f40d8a7a
Callum Rollo <c.rollo@outlook.com>
2021-04-02 11:07:11 +0100

    forgot pre-commit for the nth time

--- tests/test_physics.py
+++ tests/test_physics.py
@@ -1,10 +1,11 @@
 from glidertools.load import seaglider_basestation_netCDFs
 from glidertools.physics import (
+    brunt_vaisala,
     mixed_layer_depth,
     potential_density,
-    brunt_vaisala,
     spice0,
 )
+
 
 filenames = "./tests/data/p542*.nc"
 
@@ -25,22 +26,24 @@ ds_dict = seaglider_basestation_netCDFs(
     filenames, names, return_merged=True, keep_global_attrs=False
 )
 
-merged = ds_dict['merged']
-if 'time' in merged:
+merged = ds_dict["merged"]
+if "time" in merged:
     merged = merged.drop(["time", "time_dt64"])
-dat = merged.rename({
-    'salinity': 'salt_raw',
-    'temperature': 'temp_raw',
-    'ctd_pressure': 'pressure',
-    'ctd_depth': 'depth',
-    'ctd_time_dt64': 'time',
-    'ctd_time': 'time_raw',
-    'eng_wlbb2flvmt_wl700sig': 'bb700_raw',
-    'eng_wlbb2flvmt_wl470sig': 'bb470_raw',
-    'eng_wlbb2flvmt_Chlsig': 'flr_raw',
-    'eng_qsp_PARuV': 'par_raw',
-    'aanderaa4330_dissolved_oxygen': 'oxy_raw',
-})
+dat = merged.rename(
+    {
+        "salinity": "salt_raw",
+        "temperature": "temp_raw",
+        "ctd_pressure": "pressure",
+        "ctd_depth": "depth",
+        "ctd_time_dt64": "time",
+        "ctd_time": "time_raw",
+        "eng_wlbb2flvmt_wl700sig": "bb700_raw",
+        "eng_wlbb2flvmt_wl470sig": "bb470_raw",
+        "eng_wlbb2flvmt_Chlsig": "flr_raw",
+        "eng_qsp_PARuV": "par_raw",
+        "aanderaa4330_dissolved_oxygen": "oxy_raw",
+    }
+)
 
 
 def test_brunt_vaisala():
