fixed oxygen calculation and added oxygen tests

glidertools/processing.py
@@ -154,6 +154,7 @@ def calc_oxygen(
 
     import seawater as sw
     from numpy import abs, array, c_, isnan, median, ones
+    import numpy as np
     from pandas import Series
 
     from .cleaning import despike, outlier_bounds_iqr, savitzky_golay
@@ -204,7 +205,10 @@ def calc_oxygen(
         Y = o2raw[surf].copy()
         X = c_[ones(surf.sum()), o2sat[surf]]
         # removing outliers accodring to IQR
-        ll, ul = outlier_bounds_iqr(Y, multiplier=1.5)
+        # ll, ul = outlier_bounds_iqr(Y, multiplier=1.5)
+        iqr = outlier_bounds_iqr(Y, multiplier=1.5)
+        ll = np.nanmin(iqr)
+        ul = np.nanmax(iqr)
         m = (Y > ll) & (Y < ul)
         ratios = Y[m] / X[m, 1]
 
@@ -263,7 +267,7 @@ def calc_oxygen(
         o2aou = transfer_nc_attrs(
             getframe(),
             var,
-            o2mll,
+            o2aou,
             "o2aou",
             units="mL/L",
             comment="",
@@ -272,7 +276,7 @@ def calc_oxygen(
         o2pct = transfer_nc_attrs(
             getframe(),
             var,
-            o2mll,
+            o2pct,
             "o2pct",
             units="percent",
             comment="",