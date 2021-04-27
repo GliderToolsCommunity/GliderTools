50d540bac42006bff0439526cf9dfc64cff83aa7
Julius Busecke <julius@ldeo.columbia.edu>
2021-04-27 11:18:32 -0400

    Update test_plot.py

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -14,8 +14,7 @@ ds_dict = seaglider_basestation_netCDFs(filenames, names, keep_global_attrs=Fals
 dat = ds_dict["sg_data_point"]
 
 
-
-def test_no_warms():
+def test_no_warns():
     """Check gt_plt() raises no warnings in pcolormesh."""
     with pytest.warns(None) as record:
         gt_plt(dat.dives, dat.ctd_pressure, dat.salinity)
