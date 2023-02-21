7d9d8ec4f1e8e51b06974976489c4edff75ea6cf
Martin <37417617+MartinMohrmann@users.noreply.github.com>
2023-02-21 13:07:01 +0100

    Homogenize name of apparent oxygen utilization
    Just a name change for the test really. It's good to have the same variable name inside the oxygen processing and in the test. :)

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -12,12 +12,12 @@ from tests.test_physics import dat
 
 
 def test_calc_oxygen():
-    o2ml, o2pc, o2au = calc_oxygen(
+    o2ml, o2pc, o2aou = calc_oxygen(
         dat.oxy_raw, dat.pressure, dat.salt_raw, dat.temp_raw
     )
     assert np.nanmin(o2ml) == pytest.approx(3.7152995, 0.0001)
     assert np.nanmax(o2ml) == pytest.approx(11.460690, 0.0001)
     assert np.nanmin(o2pc) == pytest.approx(49.677466, 0.01)
     assert np.nanmax(o2pc) == pytest.approx(182.91453, 0.01)
-    assert np.nanmin(o2au) == pytest.approx(-5.195040, 0.0001)
-    assert np.nanmax(o2au) == pytest.approx(3.7637133, 0.0001)
+    assert np.nanmin(o2aou) == pytest.approx(-5.195040, 0.0001)
+    assert np.nanmax(o2aou) == pytest.approx(3.7637133, 0.0001)
